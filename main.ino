57cb75d27246d2671dc6ab334601ce0b20b5ef5e
neo-darbellay <178883466+neo-darbellay@users.noreply.github.com>
2025-05-16 14:07:54 +0200

    V1.2.0
    Changed variable names & if statements order

--- main.ino
+++ main.ino
@@ -1,66 +1,66 @@
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2); // Ajout de l'écran LCD (situé à l'adresse 0x27 écran de 16 caractères et 2 lignes) comme référence dans le code
+LiquidCrystal_I2C lcd(0x27,16,2);           // Ajout de l'écran LCD (situé à l'adresse 0x27 écran de 16 caractères et 2 lignes) comme référence dans le code
 
-const int motionPin = 9;          // Le pin du détecteur de mouvement
-const int ledMotionPin = 4;       // Le pin de la lampe LED
+const int PIN_MotionDetector = 9;           // Le pin du détecteur de mouvement
+const int PIN_LED_MotionDetector = 4;       // Le pin de la lampe LED
 
-const int buttonLedPin = 7;       // Le pin du bouton relié à une LED
-const int ledButtonPin = 2;       // Le pin de la lampe LED relié à un bouton
+const int PIN_Button_LED = 7;               // Le pin du bouton relié à une LED
+const int PIN_LED_Button = 2;               // Le pin de la lampe LED relié à un bouton
 
-const int buttonBuzzerPin = 10;   // Le pin du bouton relié au buzzer
-const int buzzerButtonPin = 8;    // Le pin du buzzer relié au bouton
+const int PIN_Button_Buzzer = 10;           // Le pin du bouton relié au buzzer
+const int PIN_Buzzer = 8;                   // Le pin du buzzer relié au bouton
 
-int buttonLedPressed = HIGH;      // Variable utilisé pour un détecteur de mouvement qui controle une LED
-int motionDetected = LOW;         // Variable utilisé pour un bouton qui controle une LED
-int buttonBuzzerPressed = HIGH;    // Variable utilisé pour un bouton qui controle un buzzer 
+int VAL_Button_LED = HIGH;                  // Variable utilisé pour un détecteur de mouvement
+int VAL_MotionDetector = LOW;               // Variable utilisé pour un bouton qui controle une LED
+int VAL_Button_Buzzer = HIGH;               // Variable utilisé pour un bouton qui controle un buzzer 
 
 void setup() {
   // Initialisation de l'écran LCD
   lcd.init();
 
   // Setup scénario 1
-  pinMode(motionPin, INPUT);
-  pinMode(ledMotionPin, OUTPUT);
+  pinMode(PIN_MotionDetector, INPUT);
+  pinMode(PIN_LED_MotionDetector, OUTPUT);
 
   // Setup scénario 2
-  pinMode(buttonLedPin, INPUT);
-  pinMode(ledButtonPin, OUTPUT);
+  pinMode(PIN_Button_LED, INPUT);
+  pinMode(PIN_LED_Button, OUTPUT);
 
   // Setup scénario 3
-  pinMode(buttonBuzzerPin, INPUT);
-  pinMode(buzzerButtonPin, OUTPUT);
+  pinMode(PIN_Button_Buzzer, INPUT);
+  pinMode(PIN_Buzzer, OUTPUT);
 }
 
 
 void Get_Values() {
-  // Lecture du signal du détecteur de mouvement qui controle une LED
-  motionDetected = digitalRead(motionPin);
+  // Lecture du signal du détecteur de mouvement
+  VAL_MotionDetector = digitalRead(PIN_MotionDetector);
 
   // Lecture du signal du bouton qui controle une LED
-  buttonLedPressed = digitalRead(buttonLedPin);
+  VAL_Button_LED = digitalRead(PIN_Button_LED);
 
   // Lecture du signal du bouton qui controle un buzzer
-  buttonBuzzerPressed = digitalRead(buttonBuzzerPin);
+  VAL_Button_Buzzer = digitalRead(PIN_Button_Buzzer);
 }
 
 void Update_States() {
   // Vérification if pour toutes les valeurs
 
-  // Scénario 2
-  if (buttonLedPressed == LOW) {
+  // Scénario 1
+  if (VAL_MotionDetector == HIGH) {
     // Allumer la LED
-    digitalWrite(ledButtonPin, HIGH);
+    digitalWrite(PIN_LED_MotionDetector, HIGH);
+  }
+  // Scénario 2
+  if (VAL_Button_LED == LOW) {
+    // Allumer la LED
+    digitalWrite(PIN_LED_Button, HIGH);
   }
   // Scénario 3
-  if (buttonBuzzerPressed == LOW) {
+  if (VAL_Button_Buzzer == LOW) {
     // Activer le buzzer (pin, fréquence en Hz, durée en ms)
-    tone(buzzerButtonPin, 200, 1000);
-  }
-  // Scénario 1
-  if (motionDetected == HIGH) {
-    // Allumer la LED
-    digitalWrite(ledMotionPin, HIGH);
+    tone(PIN_Buzzer, 200, 1000);
   }
 }
 
@@ -71,7 +71,7 @@ void Screen_Output() {
   // Vérification if else pour toutes les valeurs
 
   // Scénario 2
-  if (buttonLedPressed == LOW) {
+  if (VAL_Button_LED == LOW) {
     // Afficher un message
     lcd.backlight();
 
@@ -85,7 +85,7 @@ void Screen_Output() {
     lcd.print("soit !");
   }
   // Scénario 3
-  else if (buttonBuzzerPressed == LOW) {
+  else if (VAL_Button_Buzzer == LOW) {
     // Afficher un message
     lcd.backlight();
 
@@ -102,7 +102,7 @@ void Screen_Output() {
     lcd.print("Dong !");
   }
   // Scénario 1
-  else if (motionDetected == HIGH) {
+  else if (VAL_MotionDetector == HIGH) {
     // Afficher un message
     lcd.backlight();
 
@@ -121,18 +121,18 @@ void Handle_Delay() {
   // Vérification if else pour toutes les valeurs
 
   // Scénario 3
-  if (buttonBuzzerPressed == LOW) {
+  if (VAL_Button_Buzzer == LOW) {
     // Ajouter un délai
     delay(1500);
   }
   // Scénario 1 et 2
-  else if (buttonLedPressed == LOW || motionDetected == HIGH) {
+  else if (VAL_Button_LED == LOW || VAL_MotionDetector == HIGH) {
     // Ajouter un délai
     delay(4000);
 
     // Eteindre les deux (même si qu'une seule est allumée)
-    digitalWrite(ledButtonPin, LOW);
-    digitalWrite(ledMotionPin, LOW);
+    digitalWrite(PIN_LED_Button, LOW);
+    digitalWrite(PIN_LED_MotionDetector, LOW);
   }
 
   // Eteindre l'écran
